d93cc8eda215b4e05130900175c84fc6953e3f3c
prateek951 <prince.cenation@gmail.com>
2018-11-27 13:06:46 +0530

    [test] createTransaction endpoint

--- development/controllers/chain.js
+++ development/controllers/chain.js
@@ -1,6 +1,6 @@
-/** Created by Prateek Madaan on 27 November 2018*/ 
+/** Created by Prateek Madaan on 27 November 2018*/
 
-const Blockchain = require('../blockchain');
+const Blockchain = require("../blockchain");
 const { OK } = require("http-status-codes");
 const bitcoin = new Blockchain();
 
@@ -10,9 +10,14 @@ exports.retrieveBlockchain = (req, res, next) => {
 };
 
 exports.createTransaction = (req, res, next) => {
-//   console.log(req.body);
-  const { amount } = req.body;
-  res.status(OK).send(`The amount of the transaction is ${amount} bitcoins`);
+  /** Tap the amount, sender and the receiver from request body  */
+
+  const { amount, sender, receiver } = req.body;
+
+  const blockIndex = bitcoin.createNewTransaction(amount, sender, receiver);
+  res
+    .status(OK)
+    .json({ message: `Transaction will be added in block ${blockIndex}` });
 };
 
 exports.mineBlock = (req, res, next) => {
