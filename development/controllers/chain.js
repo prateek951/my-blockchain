b0a47a615d673d6ba42373baa14ecfa0cc56374a
prateek951 <prince.cenation@gmail.com>
2018-11-27 13:26:14 +0530

    mining the new block with success

--- development/controllers/chain.js
+++ development/controllers/chain.js
@@ -2,6 +2,13 @@
 
 const Blockchain = require("../blockchain");
 const { OK } = require("http-status-codes");
+const uuid = require("uuid/v1");
+
+/** This NODE_ADDRESS MUST BE UNIQUE */
+
+const NODE_ADDRESS = uuid()
+  .split("-")
+  .join("");
 const bitcoin = new Blockchain();
 
 exports.retrieveBlockchain = (req, res, next) => {
@@ -22,4 +29,25 @@ exports.createTransaction = (req, res, next) => {
 
 exports.mineBlock = (req, res, next) => {
   console.log("hooked up...");
+
+  const { hash: previousBlockHash, index } = bitcoin.getLastBlock();
+  const currentBlockData = {
+    transactions: bitcoin.pendingTransactions,
+    index: index + 1
+  };
+  const nonce = bitcoin.PROOF_OF_WORK(previousBlockHash, currentBlockData);
+  const blockHash = bitcoin.hashBlock(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+  );
+
+  /**  Reward the miner with 12.5 BTC*/
+  bitcoin.createNewTransaction(12.5, "00",NODE_ADDRESS);
+  /** Now mine a new block */
+  const newBlock = bitcoin.addBlock(nonce, previousBlockHash, blockHash);
+
+  res
+    .status(OK)
+    .json({ message: "New Block Mined Successfully!", block: newBlock });
 };
