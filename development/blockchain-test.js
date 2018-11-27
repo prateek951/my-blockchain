c1fb420c97b3e7900435edb2951f9a030a38a80f
prateek951 <prince.cenation@gmail.com>
2018-11-27 10:18:04 +0530

    [test] create new transaction (pending) and then mining them

--- development/blockchain-test.js
+++ development/blockchain-test.js
@@ -6,12 +6,42 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 
-/** Invoke the addBlock method on the bitcoin example instance */ 
-bitcoin.addBlock(2389,'0X2WSFVXRERE','0ACSVVWESDCFR');
-bitcoin.addBlock(239,'0X2WSadcsvfe','0ACeqqewSDCFR');
-bitcoin.addBlock(2444,'0XdafsfavsRE','0ACS3213SDCFR');
+/** Invoke the addBlock method on the bitcoin example instance */
 
-/** Invoke the createNewTransaction method on the bitcoin example instance */ 
-bitcoin.createNewTransaction(100,'Alicesfgwefhviglaedas','Bobafscdsewk3123edee')
+bitcoin.addBlock(2389, "0X2WSFVXRERE", "0ACSVVWESDCFR");
 
-console.log(bitcoin);
+/** Invoke the createNewTransaction method on the bitcoin example instance (still in pending state) */
+
+bitcoin.createNewTransaction(
+  100,
+  "Alicesfgwefhviglaedas",
+  "Bobafscdsewk3123edee"
+);
+
+/** Mine a block with the transaction that we created */
+
+bitcoin.addBlock(2444, "0XdafsfavsRE", "0ACS3213SDCFR");
+
+/** Create a new transaction (these are still pending) */
+
+bitcoin.createNewTransaction(
+  50,
+  "Alicesfgwefhviglaedas",
+  "Bobadasewdfewkdfsvjbds"
+);
+bitcoin.createNewTransaction(
+  250,
+  "Alicesfgwefhviglaedas",
+  "Bobadfsgewfvdscaewds"
+);
+bitcoin.createNewTransaction(
+  50000,
+  "Alicesfgwefhviglaedas",
+  "Biadsfsewfsacxcsewe"
+);
+
+/** Mine the new block with the transactions that we created */
+
+bitcoin.addBlock(100, "WUW4EFS32323QFCKHFE", "32UQWMDEYUGE32yzcxjzz");
+
+console.log(bitcoin.chain[2]);
